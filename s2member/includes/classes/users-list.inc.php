ca641b29f431015c7cdb1fde8963e0ba70115f87
JasWSInc <jason@websharks-inc.com>
2013-02-13 21:11:06 -0900

    * Improving readability of Last Login Time in list of Users/Members.

--- s2member/includes/classes/users-list.inc.php
+++ s2member/includes/classes/users-list.inc.php
@@ -232,7 +232,7 @@ if (!class_exists ("c_ws_plugin__s2member_users_list"))
 							if(($time = get_user_option ("s2member_last_login_time", $user_id)))
 							{
 								$time = strtotime (get_date_from_gmt (date ("Y-m-d H:i:s", $time)));
-								$val = esc_html(date ("D M jS, Y", $time)." @ precisely " . date ("g:i a", $time));
+								$val = esc_html (date ("D M jS, Y", $time)) . '<br /><small>@ precisely ' . esc_html (date ("g:i a", $time)) . '</small>';
 							}
 							else $val = "—"; // Not applicable (we've never recorded them logging into the site).
 						}
