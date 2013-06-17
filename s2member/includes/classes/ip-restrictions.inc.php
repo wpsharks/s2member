1573ea166f20bd5f5afe690230575d518380f051
JasWSInc <jason@websharks-inc.com>
2013-06-17 02:15:00 -0800

    Bug Fix
    - Fixing bug from recent addition in WebSharks/Private#148. Too many dirname() calls. Reducing this to two calls.

--- s2member/includes/classes/ip-restrictions.inc.php
+++ s2member/includes/classes/ip-restrictions.inc.php
@@ -54,7 +54,7 @@ if(!class_exists("c_ws_plugin__s2member_ip_restrictions"))
 					 $custom_template = (file_exists (TEMPLATEPATH . "/" . "ip-restrictions.php")) ? TEMPLATEPATH . "/" . "ip-restrictions.php" : false;
 					 $custom_template = (file_exists (WP_CONTENT_DIR . "/" . "ip-restrictions.php")) ? WP_CONTENT_DIR . "/" . "ip-restrictions.php" :  $custom_template;
 
-					 $msg_503 = trim(file_get_contents((($custom_template) ? $custom_template : dirname(dirname(dirname(dirname(__FILE__))))."/templates/errors/ip-restrictions.php")));
+					 $msg_503 = trim(file_get_contents((($custom_template) ? $custom_template : dirname(dirname(__FILE__))."/templates/errors/ip-restrictions.php")));
 					 $msg_503 = trim(((!$custom_template || !is_multisite() || !c_ws_plugin__s2member_utils_conds::is_multisite_farm() || is_main_site()) ? c_ws_plugin__s2member_utilities::evl($msg_503) : $msg_503));
 
                 $prefix = /* s2Member Transient prefix for all IP Restrictions. Allows s2Member to find these easily. */ "s2m_ipr_";
