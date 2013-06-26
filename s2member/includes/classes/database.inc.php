539204c1ae9412711165c14b0851fe6ab15d4b66
JasWSInc <jason@websharks-inc.com>
2013-06-25 16:45:25 -0800

    MySQL Database (wait_timeout).
    - s2Member now automatically increases the MySQL `wait_timeout` to `300` seconds during s2Member processing routines. Reason for increase: should any 3rd party service API result in unexpected connection timeouts (such as PayPal, Authorize.Net, Amazon, MailChimp, AWeber, etc); this may cause a delay that could potentially exceed the default `wait_timeout` of `30` seconds on the MySQL resource handle that is global to all of WordPress. Increasing `wait_timeout` before transaction processing will decrease the chance of failure after a timeout is exceeded. Among other things, this resolves an elusive bug where there are mysterious 404 errors after checkout under the right scenario (e.g. when an unexpected timeout occurs). This may also resolve problems associated w/ some mysterious reports where emails were not sent during s2Member's attempt to complete post-processing of a transaction (and/or where other portions of post-processing failed under rare circumstances). See also: WebSharks/Private#349

--- s2member/includes/classes/database.inc.php
+++ s2member/includes/classes/database.inc.php
@@ -11,7 +11,7 @@
 * along with this software. In the main directory, see: /licensing/
 * If not, see: {@link http://www.gnu.org/licenses/}.
 *
-* @package s2Member\No_Cache
+* @package s2Member\Database
 * @since 3.5
 */
 if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
@@ -36,14 +36,14 @@ if (!class_exists ("c_ws_plugin__s2member_database"))
 				public static function wait_timeout ()
 					{
 						global $wpdb; // Global database object reference.
-						
+
 						if(c_ws_plugin__s2member_systematics::is_s2_systematic_use_page ()
 							|| (!empty ($_SERVER["QUERY_STRING"]) && preg_match ("/[\?&]s2member/", $_SERVER["QUERY_STRING"])))
 							$increase_wait_timeout = TRUE;
-						
+
 						if(empty($increase_wait_timeout) && !empty($_POST)) foreach(array_keys($_POST) as $post_key)
 							if(strpos($post_key, "s2member") === 0) { $increase_wait_timeout = TRUE; break; }
-								
+
 						if(!empty($increase_wait_timeout)) $wpdb->query("SET SESSION `wait_timeout` = 300");
 					}
 			}
