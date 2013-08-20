127719fb278be7f14c137498959a3ee568936167
JasWSInc <jason@websharks-inc.com>
2013-08-20 03:30:26 -0800

    Improving HTTP redirection feature.

--- s2member/includes/classes/login-redirects-r.inc.php
+++ s2member/includes/classes/login-redirects-r.inc.php
@@ -30,21 +30,39 @@ if (!class_exists ("c_ws_plugin__s2member_login_redirects_r"))
 				/**
 				* Handles completely empty ``login_redirect`` values.
 				*
-				* Some plugins (most notably BuddyPress v1.5) have a nasty habit of sending an empty ``$_REQUEST["redirect_to"]`` input field with login widgets.
-				* In the case of BuddyPress, it's own Filter against `login_redirect` will deal with empty values. However, since s2Member removes all Filters
-				* against `login_redirect` (for compatibility), we NEED this simple routine to check empty values, and default them to ``admin_url()``.
-				*
 				* @attaches-to ``add_filter("login_redirect");``
 				*
 				* @package s2Member\Login_Redirects
 				* @since 110926
 				*
-				* @param str $redirect_to Expects the current ``$redirect_to`` value, passed in by the Filter.
-				* @return str A non-empty string value. s2Member will NEVER allow this to be completely empty.
+				* @param string $redirect_to Expects the current ``$redirect_to`` value, passed in by the Filter.
+				* @return string A non-empty string value. s2Member will NEVER allow this to be completely empty.
 				*/
-				public static function _empty_login_redirect_filter ($redirect_to = FALSE)
+				public static function _empty_login_redirect_filter ($redirect_to)
 					{
-						return (empty ($redirect_to)) ? admin_url () : $redirect_to;
+						return (!$redirect_to) ? admin_url () : $redirect_to;
+					}
+				/**
+				* Handles HTTP/HTTPS ``login_redirect`` values.
+				*
+				* @attaches-to ``add_filter("login_redirect");``
+				*
+				* @package s2Member\Login_Redirects
+				* @since 130819
+				*
+				* @param string $redirect_to Expects the current ``$redirect_to`` value, passed in by the Filter.
+				* @return string Updated `redirect_to` value.
+				*/
+				public static function _http_login_redirect_filter ($redirect_to)
+					{
+						if($GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["login_redirection_always_http"])
+							if($redirect_to && is_string ($redirect_to) && strpos($redirect_to, "wp-admin") === FALSE)
+								{
+									$redirect_to = preg_replace("/^https\:\/\//i", "http://", $redirect_to);
+									if(stripos($redirect_to, "http://") !== 0) // Force an absolute URL in this case.
+										$redirect_to = home_url($redirect_to, "http");
+								}
+						return $redirect_to;
 					}
 				/**
 				* Removes all other ``login_redirect`` Filters to prevent conflicts with s2Member.
@@ -53,8 +71,6 @@ if (!class_exists ("c_ws_plugin__s2member_login_redirects_r"))
 				*
 				* @package s2Member\Login_Redirects
 				* @since 3.5
-				*
-				* @return null
 				*/
 				public static function remove_login_redirect_filters ()
 					{
@@ -64,10 +80,10 @@ if (!class_exists ("c_ws_plugin__s2member_login_redirects_r"))
 							{
 								remove_all_filters /* Removes all `login_redirect` Filters. */("login_redirect");
 								add_filter ("login_redirect", "c_ws_plugin__s2member_login_redirects_r::_empty_login_redirect_filter");
+								add_filter ("login_redirect", "c_ws_plugin__s2member_login_redirects_r::_http_login_redirect_filter");
 
 								do_action ("ws_plugin__s2member_during_remove_login_redirect_filters", get_defined_vars ());
 							}
-
 						do_action ("ws_plugin__s2member_after_remove_login_redirect_filters", get_defined_vars ());
 
 						return /* Return for uniformity. */;
