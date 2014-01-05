7991f42b388a0cb7d2ce237de668747c7172c558
JasWSInc <jason@websharks-inc.com>
2014-01-05 01:10:04 -0900

    Updating changelog.

--- s2member/s2member.php
+++ s2member/s2member.php
@@ -19,8 +19,8 @@
 */
 /* -- This section for WordPress parsing. ------------------------------------------------------------------------------
 
-Version: 131126
-Stable tag: 131126
+Version: 140105
+Stable tag: 140105
 
 SSL Compatible: yes
 bbPress Compatible: yes
@@ -35,7 +35,7 @@ Authorize.Net Compatible: yes w/s2Member Pro
 Google Wallet Compatible: yes w/s2Member Pro
 ClickBank Compatible: yes w/s2Member Pro
 
-Tested up to: 3.7.1
+Tested up to: 3.8
 Requires at least: 3.3
 
 Copyright: © 2009 WebSharks, Inc.
@@ -75,7 +75,7 @@ if(realpath(__FILE__) === realpath($_SERVER["SCRIPT_FILENAME"]))
 * @var str
 */
 if(!defined("WS_PLUGIN__S2MEMBER_VERSION"))
-	define("WS_PLUGIN__S2MEMBER_VERSION", "131126" /* !#distro-version#! */);
+	define("WS_PLUGIN__S2MEMBER_VERSION", "140105" /* !#distro-version#! */);
 /**
 * Minimum PHP version required to run s2Member.
 *
@@ -105,7 +105,7 @@ if(!defined("WS_PLUGIN__S2MEMBER_MIN_WP_VERSION"))
 * @var str
 */
 if(!defined("WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION"))
-	define("WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION", "131126" /* !#distro-version#! */);
+	define("WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION", "140105" /* !#distro-version#! */);
 /*
 Several compatibility checks.
 If all pass, load the s2Member plugin.
