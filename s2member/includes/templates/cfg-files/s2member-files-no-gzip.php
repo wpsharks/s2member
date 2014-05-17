5468e2ce45d65848359f2c56fbd6485a94fbd3e6
JasWSInc <jason@websharks-inc.com>
2014-05-17 13:13:02 -0800

    Updating `<IfModule>` check to Apache 2.1+ syntax.

--- s2member/includes/templates/cfg-files/s2member-files-no-gzip.php
+++ s2member/includes/templates/cfg-files/s2member-files-no-gzip.php
@@ -8,7 +8,7 @@ $ws_plugin__s2member_temp_s_base = (!empty ($base)) ? $base : c_ws_plugin__s2mem
 // Do NOT use ``site`` URL. Must use the `home` URL here, because that's what WordPress uses in its own `mod_rewrite` implementation.
 ?>
 
-<IfModule mod_rewrite.c>
+<IfModule rewrite_module>
 	RewriteEngine On
 	RewriteBase <?php echo $ws_plugin__s2member_temp_s_base . "\n"; ?>
 	RewriteCond %{QUERY_STRING} (^|\?|&)s2member_file_download\=.+ [OR]
