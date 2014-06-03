1389913917be03fa1f2908ad0c556c86e5f40439
JasWSInc <jason@websharks-inc.com>
2014-06-03 13:32:18 -0800

    Adding styles too; https://github.com/websharks/s2member/issues/157

--- s2member/includes/templates/badges/s-badge.php
+++ s2member/includes/templates/badges/s-badge.php
@@ -2,23 +2,22 @@
 if(realpath(__FILE__) === realpath($_SERVER["SCRIPT_FILENAME"]))
 	exit("Do not access this file directly.");
 
-$v = '%%v%%';
-$dimensions = '';
-
-switch($v)
+switch('%%v%%')
 {
 	case 'v2':
-		$dimensions = 'width="180" height="58"';
+		$width_height_attr = 'width="180" height="58"';
+		$width_height_styles = 'width:180px; height:58px;';
 		break;
 	case 'v3':
-		$dimensions = 'width="80" height="15"';
+		$width_height_attr = 'width="80" height="15"';
+		$width_height_styles = 'width:80px; height:15px;';
 		break;
 	default:
-		$dimensions = 'width="200" height="55"';
+		$width_height_attr = 'width="200" height="55"';
+		$width_height_styles = 'width:200px; height:55px;';
 		break;
 }
 ?>
-
 <div class="ws-plugin--s2member-s-badge">
-	<a href="http://www.s2member.com/" onclick="window.open('http://www.s2member.com/s-badges/s-details.php?v=%%v%%&amp;site_url=%%site_url%%%%no_cache%%%%display_on_failure%%', '_popup', 'width=770,height=720,left='+((screen.width/2)-(770/2))+',screenX='+((screen.width/2)-(770/2))+',top='+((screen.height/2)-(720/2))+',screenY='+((screen.height/2)-(720/2))+',location=0,menubar=0,toolbar=0,scrollbars=0,resizable=1'); return false;" title="s2Member&reg;"><img src="//www.s2member.com/s-badges/s-badge.php?v=%%v%%&amp;site_url=%%site_url%%%%no_cache%%%%display_on_failure%%" style="border:0;" alt="s2Member&reg;" <?php echo $dimensions; ?> title="<?php echo esc_attr(_x("s2Member&reg; ( Security for WordPress&reg; )", "s2member-front", "s2member")); ?>" /></a>
+	<a href="http://www.s2member.com/" onclick="window.open('http://www.s2member.com/s-badges/s-details.php?v=%%v%%&amp;site_url=%%site_url%%%%no_cache%%%%display_on_failure%%', '_popup', 'width=770,height=720,left='+((screen.width/2)-(770/2))+',screenX='+((screen.width/2)-(770/2))+',top='+((screen.height/2)-(720/2))+',screenY='+((screen.height/2)-(720/2))+',location=0,menubar=0,toolbar=0,scrollbars=0,resizable=1'); return false;" title="s2Member&reg;"><img src="//www.s2member.com/s-badges/s-badge.php?v=%%v%%&amp;site_url=%%site_url%%%%no_cache%%%%display_on_failure%%" style="border:0; <?php echo esc_attr($width_height_styles); ?>" alt="s2Member&reg;" <?php echo $width_height_attr; ?> title="<?php echo esc_attr(_x("s2Member&reg; ( Security for WordPress&reg; )", "s2member-front", "s2member")); ?>" /></a>
 </div>
