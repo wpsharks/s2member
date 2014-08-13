819d8a9b62bbaaf8b534f4134c825ab2f4286e8b
JasWSInc <jas@wsharks.com>
2014-08-12 21:43:34 -0800

    Update player templates; but with back compat. See: websharks/s2member#179

--- s2member/includes/templates/players/jwplayer-v6.php
+++ s2member/includes/templates/players/jwplayer-v6.php
@@ -21,10 +21,7 @@ if (realpath (__FILE__) === realpath ($_SERVER["SCRIPT_FILENAME"]))
 
 					captions: %%player_captions%%,
 
-					sources: // Available sources.
-						[
-							{file: '%%url%%'}
-						]
+					sources: %%player_sources%%
 				}],
 
 			controls: %%player_controls%%,
