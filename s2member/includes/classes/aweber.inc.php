e4b21db1607cb8d20c1b843fd4637126a440517b
JasWSInc <jas@wsharks.com>
2014-10-04 05:07:07 -0800

    Refactor AWeber. See: websharks/s2member#303

--- s2member/includes/classes/aweber.inc.php
+++ s2member/includes/classes/aweber.inc.php
@@ -41,6 +41,43 @@ if(!class_exists('c_ws_plugin__s2member_aweber'))
 		{
 			if(!($args = self::validate_args($args)))
 				return FALSE; // Invalid args.
+
+			if(!$args->opt_in) // Double check.
+				return FALSE; // Must say explicitly.
+
+			if(empty($GLOBALS['WS_PLUGIN__']['s2member']['o']['level'.$args->level.'_aweber_list_ids']))
+				return FALSE; // No list configured at this level.
+
+			$args->fname       = !$args->fname ? ucwords(strstr($args->email, '@', TRUE)) : $args->fname;
+			$args->lname       = !$args->lname ? '-' : $args->lname; // Default last name to `-` because MC requires this.
+			$args->name        = $args->fname || $args->lname ? trim($args->fname.' '.$args->lname) : ucwords(strstr($args->email, '@', TRUE));
+			$aw_level_list_ids = $GLOBALS['WS_PLUGIN__']['s2member']['o']['level'.$args->level.'_aweber_list_ids'];
+
+			foreach(preg_split('/['."\r\n\t".'\s;,]+/', $aw_level_list_ids) as $_aw_list)
+			{
+				$_aw = array(
+					'args'       => $args,
+					'function'   => __FUNCTION__,
+					'list'       => trim($_aw_list),
+					'list_id'    => trim($_aw_list),
+					'api_method' => 'listSubscribe'
+				);
+				if(!$_aw['list']) continue; // List missing.
+
+				$_aw['bcc']            = apply_filters('ws_plugin__s2member_aweber_bcc', FALSE, get_defined_vars());
+				$_aw['pass_inclusion'] = apply_filters('ws_plugin__s2member_aweber_pass_inclusion', FALSE, get_defined_vars()) && $args->pass ? "\n".'Pass: '.$args->pass : '';
+
+				if($_aw['wp_mail_response'] = wp_mail($_aw['list_id'].'@aweber.com', // Converts to email address @aweber.com.
+					($_aw['wp_mail_sbj'] = apply_filters('ws_plugin__s2member_aweber_sbj', 's2Member Subscription Request', get_defined_vars())), // These filters make it possible to customize these emails.
+					($_aw['wp_mail_msg'] = apply_filters('ws_plugin__s2member_aweber_msg', 's2Member Subscription Request'."\n".'s2Member w/ PayPal Email ID'."\n".'Ad Tracking: s2Member-'.(is_multisite() && !is_main_site() ? $GLOBALS['current_blog']->domain.$GLOBALS['current_blog']->path : $_SERVER['HTTP_HOST'])."\n".'EMail Address: '.$args->email."\n".'Buyer: '.$args->name."\n".'Full Name: '.$args->name."\n".'First Name: '.$args->fname."\n".'Last Name: '.$args->lname."\n".'IP Address: '.$args->ip."\n".'User ID: '.$args->user_id."\n".'Login: '.$args->login.$_aw['pass_inclusion']."\n".'Role: '.$args->role."\n".'Level: '.$args->level."\n".'CCaps: '.$args->ccaps."\n".' - end.', get_defined_vars())),
+					($_aw['wp_mail_headers'] = 'From: "'.preg_replace('/"/', "'", $GLOBALS['WS_PLUGIN__']['s2member']['o']['reg_email_from_name']).'" <'.$GLOBALS['WS_PLUGIN__']['s2member']['o']['reg_email_from_email'].'>'.($_aw['bcc'] ? "\r\n".'Bcc: '.$_aw['bcc'] : '')."\r\n".'Content-Type: text/plain; charset=UTF-8'))
+				) $_aw['wp_mail_success'] = $success = TRUE; // Flag this as `TRUE`; assists with return value below.
+
+				c_ws_plugin__s2member_utils_logs::log_entry('aweber-api', $_aw);
+			}
+			unset($_aw_list, $_aw); // Just a little housekeeping.
+
+			return !empty($success); // If one suceeds.
 		}
 
 		/**
@@ -57,6 +94,17 @@ if(!class_exists('c_ws_plugin__s2member_aweber'))
 		{
 			if(!($args = self::validate_args($args)))
 				return FALSE; // Invalid args.
+
+			if(!$args->opt_out) // Double check.
+				return FALSE; // Must say explicitly.
+
+			if(empty($GLOBALS['WS_PLUGIN__']['s2member']['o']['level'.$args->level.'_aweber_list_ids']))
+				return FALSE; // No list configured at this level.
+
+			$args->fname       = !$args->fname ? ucwords(strstr($args->email, '@', TRUE)) : $args->fname;
+			$args->lname       = !$args->lname ? '-' : $args->lname; // Default last name to `-` because MC requires this.
+			$args->name        = $args->fname || $args->lname ? trim($args->fname.' '.$args->lname) : ucwords(strstr($args->email, '@', TRUE));
+			$aw_level_list_ids = $GLOBALS['WS_PLUGIN__']['s2member']['o']['level'.$args->level.'_aweber_list_ids'];
 		}
 
 		/**
