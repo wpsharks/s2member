32c4cc1659e5e4bcef226f86a5a371f783d3962a
jaswsinc <jas@wsharks.com>
2015-02-25 21:04:15 -0900

    Updating directory checksums.

--- s2member/s2member.php
+++ s2member/s2member.php
@@ -19,8 +19,8 @@
  */
 /* -- This section for WordPress parsing. ------------------------------------------------------------------------------
 
-Version: 150223
-Stable tag: 150223
+Version: 150225
+Stable tag: 150225
 
 SSL Compatible: yes
 bbPress Compatible: yes
@@ -76,7 +76,7 @@ if(!defined('WPINC')) // MUST have WordPress.
  * @var string
  */
 if(!defined('WS_PLUGIN__S2MEMBER_VERSION'))
-	define('WS_PLUGIN__S2MEMBER_VERSION', '150223' /* !#distro-version#! */);
+	define('WS_PLUGIN__S2MEMBER_VERSION', '150225' /* !#distro-version#! */);
 /**
  * Minimum PHP version required to run s2Member.
  *
@@ -106,7 +106,7 @@ if(!defined('WS_PLUGIN__S2MEMBER_MIN_WP_VERSION'))
  * @var string
  */
 if(!defined('WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION'))
-	define('WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION', '150223' /* !#distro-version#! */);
+	define('WS_PLUGIN__S2MEMBER_MIN_PRO_VERSION', '150225' /* !#distro-version#! */);
 /*
 Several compatibility checks.
 If all pass, load the s2Member plugin.
