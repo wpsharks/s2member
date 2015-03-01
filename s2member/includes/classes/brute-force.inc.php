e8de3afa7c02d81244c9c5720d646f3242a11294
jaswsinc <jas@wsharks.com>
2015-03-01 11:05:40 -0900

    Introducing `$GLOBALS['s2member_pro_remote_op_auth_check_user_ip']` for improved Brute-Force detection when a user attempts to authenticate via the Pro Remote Operations API.

--- s2member/includes/classes/brute-force.inc.php
+++ s2member/includes/classes/brute-force.inc.php
@@ -15,9 +15,9 @@
  * @since 3.5
  */
 if(!defined('WPINC')) // MUST have WordPress.
-	exit("Do not access this file directly.");
+	exit('Do not access this file directly.');
 
-if(!class_exists("c_ws_plugin__s2member_brute_force"))
+if(!class_exists('c_ws_plugin__s2member_brute_force'))
 {
 	/**
 	 * s2Member's Brute Force protection routines.
@@ -36,26 +36,31 @@ if(!class_exists("c_ws_plugin__s2member_brute_force"))
 		 * @package s2Member\Brute_Force
 		 * @since 3.5
 		 *
-		 * @attaches-to ``add_action("wp_login_failed");``
+		 * @attaches-to ``add_action('wp_login_failed');``
 		 *
 		 * @param string $username Expects the $username to be passed in through the Hook.
 		 */
 		public static function track_failed_logins($username = '')
 		{
 			foreach(array_keys(get_defined_vars()) as $__v) $__refs[$__v] =& $$__v;
-			do_action("ws_plugin__s2member_before_track_failed_logins", get_defined_vars());
+			do_action('ws_plugin__s2member_before_track_failed_logins', get_defined_vars());
 			unset($__refs, $__v);
 
-			if(($max = $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["max_failed_login_attempts"]))
-				if(!c_ws_plugin__s2member_pro_remote_ops::is_remote_op('auth_check_user'))
-				{
-					$exp_secs = strtotime("+".apply_filters("ws_plugin__s2member_track_failed_logins__exp_time", "30 minutes", get_defined_vars())) - time();
-					// If you add Filters to this value, you should use a string that is compatible with PHP's strtotime() function.
+			if(($max = $GLOBALS['WS_PLUGIN__']['s2member']['o']['max_failed_login_attempts']))
+			{
+				$exp_secs = strtotime('+'.apply_filters('ws_plugin__s2member_track_failed_logins__exp_time', '30 minutes', get_defined_vars())) - time();
+				// If you add Filters to this value, you should use a string that is compatible with PHP's strtotime() function.
 
-					$transient = "s2m_ipr_".md5("s2member_transient_failed_login_attempts_".$_SERVER["REMOTE_ADDR"]);
-					set_transient($transient, (int)get_transient($transient) + 1, $exp_secs);
-				}
-			do_action("ws_plugin__s2member_after_track_failed_logins", get_defined_vars());
+				$ip = $_SERVER['REMOTE_ADDR']; // Default value.
+				if(!empty($GLOBALS['s2member_pro_remote_op_auth_check_user_ip'])
+				   && c_ws_plugin__s2member_utils_conds::pro_is_installed()
+				   && c_ws_plugin__s2member_pro_remote_ops::is_remote_op('auth_check_user')
+				) $ip = $GLOBALS['s2member_pro_remote_op_auth_check_user_ip'];
+
+				$transient = 's2m_ipr_'.md5('s2member_transient_failed_login_attempts_'.$ip);
+				set_transient($transient, (int)get_transient($transient) + 1, $exp_secs);
+			}
+			do_action('ws_plugin__s2member_after_track_failed_logins', get_defined_vars());
 		}
 
 		/**
@@ -67,7 +72,7 @@ if(!class_exists("c_ws_plugin__s2member_brute_force"))
 		 * @package s2Member\Brute_Force
 		 * @since 3.5
 		 *
-		 * @attaches-to ``add_filter("authenticate");``
+		 * @attaches-to ``add_filter('authenticate');``
 		 *
 		 * @param WP_User $user Expects a WP_User object, or possibly a null value.
 		 *   This parameter value is simply passed through this routine.
@@ -77,25 +82,31 @@ if(!class_exists("c_ws_plugin__s2member_brute_force"))
 		public static function stop_brute_force_logins($user = NULL)
 		{
 			foreach(array_keys(get_defined_vars()) as $__v) $__refs[$__v] =& $$__v;
-			do_action("ws_plugin__s2member_before_stop_brute_force_logins", get_defined_vars());
+			do_action('ws_plugin__s2member_before_stop_brute_force_logins', get_defined_vars());
 			unset($__refs, $__v);
 
-			if(($max = $GLOBALS["WS_PLUGIN__"]["s2member"]["o"]["max_failed_login_attempts"]))
-				if(!c_ws_plugin__s2member_pro_remote_ops::is_remote_op('auth_check_user'))
-				{
-					if((int)get_transient("s2m_ipr_".md5("s2member_transient_failed_login_attempts_".$_SERVER["REMOTE_ADDR"])) > $max)
-					{
-						$exp_secs = strtotime("+".apply_filters("ws_plugin__s2member_track_failed_logins__exp_time", "30 minutes", get_defined_vars())) - time();
-						// If you add Filters to this value, you should use a string that is compatible with PHP's strtotime() function.
-						$about  = c_ws_plugin__s2member_utils_time::approx_time_difference(time(), time() + $exp_secs);
-						$errors = new WP_Error ("incorrect_password", sprintf(_x("Max failed logins. Please wait %s and try again.", "s2member-front", "s2member"), $about));
+			if(($max = $GLOBALS['WS_PLUGIN__']['s2member']['o']['max_failed_login_attempts']))
+			{
+				$ip = $_SERVER['REMOTE_ADDR']; // Default value.
+				if(!empty($GLOBALS['s2member_pro_remote_op_auth_check_user_ip'])
+				   && c_ws_plugin__s2member_utils_conds::pro_is_installed()
+				   && c_ws_plugin__s2member_pro_remote_ops::is_remote_op('auth_check_user')
+				) $ip = $GLOBALS['s2member_pro_remote_op_auth_check_user_ip'];
 
-						foreach(array_keys(get_defined_vars()) as $__v) $__refs[$__v] =& $$__v;
-						do_action("ws_plugin__s2member_during_stop_brute_force_logins", get_defined_vars());
-						unset($__refs, $__v);
-					}
+				if((int)get_transient('s2m_ipr_'.md5('s2member_transient_failed_login_attempts_'.$ip)) > $max)
+				{
+					$exp_secs = strtotime('+'.apply_filters('ws_plugin__s2member_track_failed_logins__exp_time', '30 minutes', get_defined_vars())) - time();
+					// If you add Filters to this value, you should use a string that is compatible with PHP's strtotime() function.
+
+					$about  = c_ws_plugin__s2member_utils_time::approx_time_difference(time(), time() + $exp_secs);
+					$errors = new WP_Error('incorrect_password', sprintf(_x('Max failed logins. Please wait %s and try again.', 's2member-front', 's2member'), $about));
+
+					foreach(array_keys(get_defined_vars()) as $__v) $__refs[$__v] =& $$__v;
+					do_action('ws_plugin__s2member_during_stop_brute_force_logins', get_defined_vars());
+					unset($__refs, $__v);
 				}
-			return apply_filters("ws_plugin__s2member_stop_brute_force_logins", ((!empty($errors)) ? $errors : $user), get_defined_vars());
+			}
+			return apply_filters('ws_plugin__s2member_stop_brute_force_logins', !empty($errors) ? $errors : $user, get_defined_vars());
 		}
 	}
 }
