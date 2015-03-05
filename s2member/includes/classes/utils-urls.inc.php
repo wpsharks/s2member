b3b2ec419a8eb329be90add0bee931401f5568c6
jaswsinc <jas@wsharks.com>
2015-03-04 21:00:49 -0900

    Force WP `sslverify` off on localhost installs to help others avoid confusion. See: https://github.com/websharks/s2member/issues/504 See also: http://wordpress.stackexchange.com/questions/167898/is-it-save-to-use-sslverify-true-for-with-wp-remote-get-wp-remote-post

--- s2member/includes/classes/utils-urls.inc.php
+++ s2member/includes/classes/utils-urls.inc.php
@@ -198,6 +198,9 @@ if(!class_exists("c_ws_plugin__s2member_utils_urls"))
 
 								$args["httpversion"] = (!isset($args["httpversion"])) ? "1.1" : $args["httpversion"];
 
+								if(!isset($args["sslverify"]) && c_ws_plugin__s2member_utils_conds::is_localhost())
+									$args["sslverify"] = FALSE; // Force this off on localhost installs.
+
 								if((is_array($post_vars) || is_string($post_vars)) && !empty($post_vars))
 									$args = array_merge($args, array("method" => "POST", "body" => $post_vars));
 
