88a05edbae540c2b817bf96d36af8acb2a2af8ab
jaswsinc <jas@wsharks.com>
2015-05-18 13:25:24 -0800

    Preserve GA vars. See: https://github.com/websharks/s2member/issues/570

--- s2member/includes/syscon.inc.php
+++ s2member/includes/syscon.inc.php
@@ -177,6 +177,7 @@ if(!function_exists('ws_plugin__s2member_configure_options_and_their_defaults'))
 		$default_options['login_redirection_always_http']       = '1';
 		$default_options['membership_options_page']             = '';
 		$default_options['membership_options_page_vars_enable'] = '1';
+		$default_options['membership_options_page_ga_vars_enable'] = '1';
 
 		$default_options['login_reg_design_enabled'] = '1';
 
@@ -465,6 +466,9 @@ if(!function_exists('ws_plugin__s2member_configure_options_and_their_defaults'))
 					$value = $default_options[$key];
 
 				else if($key === 'membership_options_page_vars_enable' && (!is_string($value) || !is_numeric($value)))
+					$value = $default_options[$key];
+
+				else if($key === 'membership_options_page_ga_vars_enable' && (!is_string($value) || !is_numeric($value)))
 					$value = $default_options[$key];
 
 				else if($key === 'login_reg_design_enabled' && (!is_string($value) || !is_numeric($value)))
