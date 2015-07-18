55533d1475c0237e5c263d79fe17a7716d5d832f
jaswsinc <jas@wsharks.com>
2015-07-17 18:08:12 -0800

    Bug fix in list servers via profile. See: https://github.com/websharks/s2member/issues/633

--- s2member/includes/classes/profile-mods-in.inc.php
+++ s2member/includes/classes/profile-mods-in.inc.php
@@ -136,7 +136,7 @@ if(!class_exists('c_ws_plugin__s2member_profile_mods_in'))
 					{
 						c_ws_plugin__s2member_list_servers::process_list_servers($role, $level, $user->user_login, ((!empty($userdata['user_pass'])) ? $userdata['user_pass'] : ''), $user->user_email, $user->first_name, $user->last_name, $_SERVER['REMOTE_ADDR'], TRUE, TRUE, $user_id);
 					}
-					else if($role && $level >= 0)
+					else if($role && $level >= 0 && $GLOBALS['WS_PLUGIN__']['s2member']['o']['custom_reg_opt_in'])
 					{
 						c_ws_plugin__s2member_list_servers::process_list_server_removals($role, $level, $user->user_login, ((!empty($userdata['user_pass'])) ? $userdata['user_pass'] : ''), $user->user_email, $user->first_name, $user->last_name, $_SERVER['REMOTE_ADDR'], TRUE, $user_id);
 					}
