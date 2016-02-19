240a14836f946d0d00eb22e544dbebd088ed24b5
renzms <renz@websharks-inc.com>
2016-02-19 15:52:49 +0800

    Added Visual User Interface; see: websharks/s2member#791

--- s2member/includes/menu-pages/down-ops.inc.php
+++ s2member/includes/menu-pages/down-ops.inc.php
@@ -505,6 +505,24 @@ if(!class_exists("c_ws_plugin__s2member_menu_page_down_ops"))
 				echo '<tbody>'."\n";
 				echo '<tr>'."\n";
 
+				echo '<th>'."\n";
+				echo '<label for="ws-plugin--s2member-amazon-cf-files-rtmp-policy-include-ip">'."\n";
+				echo 'Exclude IPs from digitally signed links as a part of the CloudFront RTMP streams?'."\n";
+				echo '</label>'."\n";
+				echo '</th>'."\n";
+
+				echo '</tr>'."\n";
+				echo '<tr>'."\n";
+
+				echo '<td>'."\n";
+				echo '<input type="checkbox" name="ws_plugin__s2member_amazon_cf_files_rtmp_policy_include_ip" id="ws-plugin--s2member-amazon-cf-files-rtmp-policy-include-ip" value="'.esc_attr(wp_create_nonce("ws-plugin--s2member-amazon-cf-files-rtmp-policy-include-ip")).'"'.((!empty(c_ws_plugin__s2member_menu_pages::$pre_display_errors["cf_files_rtmp_policy_include_ip"])) ? ' checked="checked"' : '').' /> <label for="ws-plugin--s2member-amazon-cf-files-rtmp-policy-include-ip"><strong>Yes</strong>, allow IPs to be excluded from digitally signed links that lead to CloudFront RTMP streams.</label><br />'."\n";
+				echo '<em>s2Member will disable IPs as a part of the digitally signed links (for RTMP streams) and will improve compatibility across various devices and/or ISPs.</em>'."\n";
+				echo '</td>'."\n";
+
+				echo '<table class="form-table" style="margin-top:0;">'."\n";
+				echo '<tbody>'."\n";
+				echo '<tr>'."\n";
+
 				echo '<td>'."\n";
 				echo '<h3>Completely Reset CloudFront Configuration?</h3>'."\n";
 				echo '<div style="float:right; margin:0 0 0 25px;">'."\n";
