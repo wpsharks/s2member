2c50d51d8378dc51f630fa07227b93c50c6097ae
jaswsinc <me@jaswsinc.com>
2016-11-17 03:00:55 -0900

    Adding phing build system.

---
+++ assets/s2-server-scanner.php
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e57ee81d476599a6aaefe779c8c7e02d2c24f60fd1aa6cdc8157c8c28f6f500b
+size 170301
---
+++ images/README.txt
@@ -0,0 +1,1 @@
+These images exist for backward compatibility with existing s2Member installations.
