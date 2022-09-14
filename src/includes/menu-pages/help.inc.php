a347b8223cb34875ec1989633623b0cbeac4dabf
clavaque <clavaque@gmail.com>
2022-09-14 04:11:55 +0200

    Changes to Help admin page

--- src/includes/menu-pages/help.inc.php
+++ src/includes/menu-pages/help.inc.php
@@ -38,7 +38,7 @@ if (!class_exists ("c_ws_plugin__s2member_menu_page_help"))
 						c_ws_plugin__s2member_menu_pages_tb::display ();
 						echo '</div>'."\n";
 
-						echo '<h2>Getting Help w/ s2Member®</h2>' . "\n";
+						echo '<h2>Getting Help with s2Member</h2>' . "\n";
 
 						echo '<table class="ws-menu-page-table">' . "\n";
 						echo '<tbody class="ws-menu-page-table-tbody">' . "\n";
@@ -48,29 +48,38 @@ if (!class_exists ("c_ws_plugin__s2member_menu_page_help"))
 						do_action("ws_plugin__s2member_during_help_page_before_left_sections", get_defined_vars ());
 						do_action("ws_plugin__s2member_during_help_page_during_left_sections_before_help", get_defined_vars ());
 
-						echo '<div class="ws-menu-page-group" title="Getting Help w/ s2Member" default-state="open">' . "\n";
+						echo '<div class="ws-menu-page-group" title="Getting Help with s2Member" default-state="open">' . "\n";
 
 						echo '<div class="ws-menu-page-section ws-plugin--s2member-help">' . "\n";
-						echo '<p>s2Member is pretty easy to setup and install initially. Most of the official documentation is right here in your Dashboard (i.e., there is a lot of inline documentation built into the software). That being said, it can take some time to master everything there is to know about s2Member\'s advanced features. If you need assistance with s2Member, please search the <a href="http://s2member.com/kb/" target="_blank" rel="external">s2Member Knowledge Base</a>, <a href="http://s2member.com/r/videos/" target="_blank" rel="external">Video Tutorials</a>, <a href="http://s2member.com/r/forum/" target="_blank" rel="external">Forums</a> and <a href="http://s2member.com/r/codex/" target="_blank" rel="external">Codex</a>. If you are planning to do something creative with s2Member, you might want to <a href="https://s2member.com/r/jobs-wordpress-net/" target="_blank" rel="external">hire a freelance developer</a> to assist you.</p>' . "\n";
-						echo '<p><strong>See also:</strong> <a href="http://s2member.com/r/common-troubleshooting-tips/" target="_blank" rel="external">s2Member Troubleshooting Guide</a> (please read this first if you\'re having trouble).</p>'."\n";
+						echo '<p>s2Member is pretty easy to setup and install initially. Most of the official documentation is right here in your Dashboard (there is a lot of inline documentation built into the software). If you need assistance with s2Member, please try searching <a href="https://google.com" target="_blank" rel="external">the web</a>, our <a href="http://s2member.com/kb/" target="_blank" rel="external">knowledge base</a>, <a href="http://s2member.com/r/videos/" target="_blank" rel="external">videos</a>, <a href="http://s2member.com/r/forum/" target="_blank" rel="external">forums</a>, or <a href="http://s2member.com/r/codex/" target="_blank" rel="external">codex</a>.' . "\n";
 
 						echo '<div class="ws-menu-page-hr"></div>' . "\n";
 
-						echo '<h3 style="margin-bottom:0;">Testing Server Compatibility</h3>'."\n";
-						echo '<p>Please download the <a href="http://s2member.com/r/server-scanner-info/">s2Member Server Scanner</a>. Unzip, upload via FTP; then open in a browser for a full report.</p>'."\n";
+						echo '<h3 style="margin-bottom:0;">Troubleshooting Tips</h3>'."\n";
+						echo '<p>Follow this <a href="http://s2member.com/r/common-troubleshooting-tips/" target="_blank" rel="external">Troubleshooting Guide</a>. Please read this first if you\'re having trouble, it has great information on how to troubleshoot.</p>'."\n";
 
 						echo '<div class="ws-menu-page-hr"></div>' . "\n";
 
-						echo '<h3 style="margin-bottom:0;">Troubleshooting Payment Gateway Integrations</h3>'."\n";
-						echo '<p>Please use s2Member\'s <a href="'.esc_attr(admin_url("/admin.php?page=ws-plugin--s2member-logs")).'">Log Viewer</a>. Log files can be very helpful.</p>'."\n";
+						echo '<h3 style="margin-bottom:0;">Enable Logging</h3>'."\n";
+						echo '<p>Please use <a href="'.esc_attr(admin_url("/admin.php?page=ws-plugin--s2member-logs")).'">s2Member\'s logs</a>. These log files can be very helpful while troubleshooting.</p>'."\n";
 
 						echo '<div class="ws-menu-page-hr"></div>' . "\n";
 
-						echo '<h3 style="margin-bottom:0;">Search s2Member KB Articles<em>!</em></h3>'."\n";
+						echo '<h3 style="margin-bottom:0;">Search s2Member KB Articles</h3>'."\n";
 						echo '<form method="get" action="http://s2member.com/kb/" target="_blank" onsubmit="if(this.q.value === \'enter search terms...\') this.q.value = \'\';" autocomplete="off">'."\n";
 						echo '<p><input type="text" name="kb_q" value="enter search terms..." style="width:60%;" onfocus="if(this.value === \'enter search terms...\') this.value = \'\';" onblur="if(this.value === \'\') this.value = \'enter search terms...\';" /> <input type="submit" value="Search" style="font-size:120%; font-weight:normal;" /></p>'."\n";
 						echo '</form>'."\n";
 
+						echo '<div class="ws-menu-page-hr"></div>' . "\n";
+
+						echo '<h3 style="margin-bottom:0;">Do a Web Search</h3>'."\n";
+
+						echo '<p></p><p>A <a href="https://google.com" target="_blank" rel="external">Google search</a> for s2member and some keywords for what you\'re trying to solve/answer, is usually very helpful. You\'ll often get links to threads in our old forums, which are packed with helpful information, sometimes not easily found by other means.</p>'."\n";
+
+						echo '<form method="get" action="https://google.com/search" target="_blank" onsubmit="this.q.value = \'s2member \' + this.q.value;" autocomplete="off">'."\n";
+						echo '<p><input type="text" name="q" value="enter search terms..." style="width:60%;" onfocus="if(this.value === \'enter search terms...\') this.value = \'\';" onblur="if(this.value === \'\') this.value = \'enter search terms...\';" /> <input type="submit" value="Search" style="font-size:120%; font-weight:normal;" /></p>'."\n";
+						echo '</form>'."\n";
+	
 						do_action("ws_plugin__s2member_during_start_page_during_left_sections_during_help", get_defined_vars ());
 
 						echo '</div>' . "\n";
@@ -81,16 +90,13 @@ if (!class_exists ("c_ws_plugin__s2member_menu_page_help"))
 
 						do_action("ws_plugin__s2member_during_help_page_during_left_sections_before_support", get_defined_vars ());
 
-						echo '<div class="ws-menu-page-group" title="s2Member Tech. Support (for Pro Customers)" default-state="open">' . "\n";
+						echo '<div class="ws-menu-page-group" title="s2Member Support" default-state="open">' . "\n";
 
 						echo '<div class="ws-menu-page-section ws-plugin--s2member-support">' . "\n";
-						echo '<p style="max-width:900px;">Support for s2Member® is provided by WebSharks, Inc. Our customer support representatives are available Monday through Friday, excluding all major holidays. Or, you can discuss problems/solutions with others in our <a href="http://s2member.com/r/forums/" target="_blank" rel="external">public community forum</a>.</p>'."\n";
+						echo '<p style="max-width:900px;">Support for s2Member is provided by WP Sharks. Join us in our <a href="http://s2member.com/r/forums/" target="_blank" rel="external">community forum</a>.</p>'."\n";
 
 						echo '<ul>'."\n";
-						echo '<li>For pre-sale questions please see: <a href="http://s2member.com/kb/kb-tag/pre-sale-faqs/" target="_blank" rel="external">Pre-Sale FAQs</a>.</li>'."\n";
-						echo '<li>For installation instructions, please see: <a href="http://s2member.com/installation/" target="_blank" rel="external">Installing/Updating s2Member & s2Member Pro</a>.</li>'."\n";
-						echo '<li>For troubleshooting (and documentation) please <a href="http://s2member.com/kb/" target="_blank" rel="external">search our Knowledge Base</a>.</li>'."\n";
-						echo '<li>Paying customers in need of assistance may <a href="http://s2member.com/r/new-trouble-ticket/" target="_blank" rel="external">submit a trouble ticket</a>.</li>'."\n";
+						echo '<li>For pre-sale questions, please see: <a href="http://s2member.com/kb/kb-tag/pre-sale-faqs/" target="_blank" rel="external">Pre-Sale FAQs</a>.</li>'."\n";
 						echo '<li>If you have other questions, please <a href="http://s2member.com/r/new-pre-sale-inquiry/" target="_blank" rel="external">contact our sales dept</a>.</li>'."\n";
 						echo '</ul>'."\n";
 
