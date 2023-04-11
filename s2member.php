9c1bdd0f8cc048438243f32b91e0211e8ba77980
clavaque <clavaque@gmail.com>
2023-04-11 14:06:56 +0200

    Update upgrade promo notice in admin

--- s2member.php
+++ s2member.php
@@ -187,29 +187,31 @@ else if(is_admin()) // Admin compatibility errors.
 }
 unset(${__FILE__}); // Housekeeping.
 
-//221026 Promo boo
+//230411 Promo upgrade
 if (is_admin() && !defined('WS_PLUGIN__S2MEMBER_PRO_VERSION')) {
 	// Dismiss
 	add_action('admin_init', function(){
 		$user_id = get_current_user_id();
-		if (isset($_GET['s2-dismiss-boo22']))
-				add_user_meta($user_id, 's2_notice_dismissed_boo22_3', 'true', true);
+		if (isset($_GET['s2-dismiss-230411']))
+				add_user_meta($user_id, 's2_notice_dismissed_230411', 'true', true);
 	});
 	// Notice
 	add_action('admin_notices', function(){
 		$user_id = get_current_user_id();
 		$logo_url = $GLOBALS['WS_PLUGIN__']['s2member']['c']['dir_url'].'/src/images/logo-square-big.png';
-		$dismiss_url = add_query_arg('s2-dismiss-boo22', '', $_SERVER['REQUEST_URI']);
-		$color = '#ff6000';
-		if (isset($_GET['s2-show-notice']) || !get_user_meta($user_id, 's2_notice_dismissed_boo22_3')) {
+		$dismiss_url = add_query_arg('s2-dismiss-230411', '', $_SERVER['REQUEST_URI']);
+		$color1 = '#009bff'; // blue
+		$color2 = '#89ccf6'; // blue glow
+		$color3 = '#ff00c3'; // pink
+		if (isset($_GET['s2-show-notice']) || !get_user_meta($user_id, 's2_notice_dismissed_230411')) {
 			echo '
-				<div class="notice" style="position:relative; border-left-color:'.$color.'; box-shadow: 0px 0px 6px 0px '.$color.' !important;">
+				<div class="notice" style="position:relative; border-left-color:'.$color2.'; box-shadow: 0px 0px 6px 0px '.$color1.' !important;">
 					<table><tr>
-					<td><a href="https://s2member.com/" target="_blank"><img src="'.$logo_url.'" height="70" width="70" align="top" style="padding-right:1em; filter: hue-rotate(-160deg) saturate(60) brightness(150%); -webkit-filter: hue-rotate(-160deg) saturate(60) brightness(150%);" /></a></td>
+					<td><a href="https://s2member.com/" target="_blank"><img src="'.$logo_url.'" height="70" width="70" align="top" style="padding-right:1em; filter: hue-rotate(0deg) saturate(80) brightness(100%); -webkit-filter: hue-rotate(0deg) saturate(80) brightness(100%);" /></a></td>
 					<td>
-						<span style="font-style:italic;">I\'m very happy you\'re using s2Member! 💕 so I discounted <a href="https://s2member.com/prices" target="_blank" style="color:'.$color.' !important; font-weight:bold;">20% OFF s2Member Pro</a> for you, if you get it now...<br />
-						Get paid more with <a href="https://s2member.com/testimonials/" target="_blank" style="color:'.$color.' !important; font-weight:bold;">s2Member Pro!</a> with Stripe on-site payments, success redirections, reminder emails, <a href="https://s2member.com/features/" target="_blank">and more!</a></span><br />
-						<b><i>This is a limited offer and expires soon...</i> ➡️ <i><a href="https://s2member.com/prices/" target="_blank">Secure your lifetime license NOW at the best price!</a></i></b> ⬅️ 🙂<br />
+						<span style="font-style:italic;"><b>I\'m very happy you\'re using s2Member!</b> 💕 so I discounted <a href="https://s2member.com/prices" target="_blank" style="color:'.$color3.' !important; font-weight:bold;">20% OFF s2Member Pro</a> for you, if you get it now...<br />
+						Make more money with <a href="https://s2member.com/testimonials/" target="_blank" style="color:'.$color3.' !important; font-weight:bold;">s2Member Pro!</a> with on-site payments, success redirections, reminder emails, <a href="https://s2member.com/features/" target="_blank">and more!</a></span><br />
+						<b><i>This is a limited offer and expires soon...</i> ➡️ <i><a href="https://s2member.com/prices/" target="_blank" style="color:'.$color1.' !important;">Secure your lifetime license NOW at the best price!</a></i></b>&nbsp; ⬅️ 😀<br />
 					</td>
 					</tr></table>
 					<a href="'.$dismiss_url.'" class="notice-dismiss" style="text-decoration:none;"><span class="screen-reader-text">Dismiss this notice.</span></a>
