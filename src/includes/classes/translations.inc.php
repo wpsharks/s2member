7af93efe1bfeafeaff074168ffa03f1eb1356a70
clavaque <clavaque@gmail.com>
2026-09-17 05:46:11 +0200

    Fix BuddyPress helper guard in s2Member-Only loader

--- src/includes/classes/translations.inc.php
+++ src/includes/classes/translations.inc.php
@@ -155,8 +155,8 @@ if(!class_exists("c_ws_plugin__s2member_translations"))
 					}
 				}
 			}
-			else if((isset ($s["is_bp_blog_creation"]) && $s["is_bp_blog_creation"]) || (!isset ($s["is_bp_blog_creation"]) && ($s["is_bp_blog_creation"] = (c_ws_plugin__s2member_utils_conds::bp_is_installed() && bp_is_create_blog()) ? TRUE : FALSE)))
-			{
+			//260917 Guard optional BuddyPress helper; it may be unavailable while s2Member-Only is loading.
+			else if((isset ($s["is_bp_blog_creation"]) && $s["is_bp_blog_creation"]) || (!isset ($s["is_bp_blog_creation"]) && ($s["is_bp_blog_creation"] = (c_ws_plugin__s2member_utils_conds::bp_is_installed() && function_exists('bp_is_create_blog') && bp_is_create_blog()) ? TRUE : FALSE)))			{
 				if($original === "If you&#8217;re not going to use a great domain, leave it for a new user. Now have at it!")
 				{
 					$translated = apply_filters("ws_plugin__s2member_translation_mangler", "", get_defined_vars());
